d550144406bd666fbf7625b2843cac563c2768d5
Obi Fortune <gabrielobi.of@gmail.com>
2022-02-17 19:18:18 +0200

    updated git searches

--- config/fish/functions/peco_change_directory.fish
+++ config/fish/functions/peco_change_directory.fish
@@ -16,9 +16,9 @@ function peco_change_directory
     begin
         echo $HOME/.config
         ghq list -p
-        ls -ad */ | perl -pe "s#^#$PWD/#" | grep -v \.git # Gets the repositories in the current directory
+        # ls -ad */ | perl -pe "s#^#$PWD/#" | grep -v \.git # Gets the repositories in the current directory
         ls -ad $HOME/apps/* | grep -v \.git
-        ls -ad $DOCS/IB | grep -v \.git
+        find $DOCS/IB -maxdepth 2 -type d \( -execdir test -e {}/.ignore \; -prune \) -o \( -execdir test -e {}/"lerna.json" \; -prune -print \) -o \( -execdir test -d {}/.git \; -prune -print \)
         find $DOCS/Github -maxdepth 2 -type d \( -execdir test -e {}/.ignore \; -prune \) -o \( -execdir test -e {}/"lerna.json" \; -prune -print \) -o \( -execdir test -d {}/.git \; -prune -print \)
     end | sed -e 's/\/$//' | awk '!a[$0]++' | _peco_change_directory $argv
 end
